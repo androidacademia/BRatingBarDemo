1cde31aefef0d3cb87ea64676e38dbd19f9f5f35
Girish Kumar Shakya <clairvoyant.1947@gmail.com>
2018-12-27 17:40:31 +0530

    Initial commit

---
+++ app/src/main/java/helloworld/com/ratingbardemo/MainActivity.java
@@ -0,0 +1,24 @@
+package helloworld.com.ratingbardemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private RatingBar ratingBar;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ratingBar = findViewById(R.id.ratingBar);
+        textView = findViewById(R.id.textView);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                textView.setText("Rating value "+v);
+            }
+        });
+    }
+}
